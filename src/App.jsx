59533ea76c6b9e63d2cb7ca2a6dd592aab37be32
anonymous-lad2 <anonymouslad540@gmail.com>
2024-10-07 05:27:56 +0530

    Added buttton and header components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import ButtonGradient from './assets/svg/ButtonGradient'
+import { Button } from './components/Button'
+import { Header } from './components/Header'
 
-function App() {
+const App = () => {
 
   return (
-    <h1 className='text-3xl font-bold underline'>
-      Hello World!
-    </h1>
+    <>
+      <h1 className='text-3xl font-bold underline'>Hello World!</h1>
+
+      <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
+        <Header />
+      </div>
+      <ButtonGradient />
+    </>
   )
+
 }
 
 export default App
