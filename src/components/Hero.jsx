0bea5d6715d905f05b3823dddd2d69a2af069c0e
anonymous-lad2 <anonymouslad540@gmail.com>
2024-10-09 16:53:28 +0530

    Added Notification component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,6 +6,7 @@ import { heroIcons } from "../constants";
 import { ScrollParallax } from "react-just-parallax";
 import { useRef } from "react";
 import { Generating } from "./Generating";
+import { Notification } from "./Notification";
 
 export const Hero = () => {
   const parallaxRef = useRef(null);
@@ -68,7 +69,10 @@ export const Hero = () => {
                 </ScrollParallax>
 
                 <ScrollParallax isAbsolutelyPositioned>
-                  
+                  <Notification
+                    className="hidden absolute -right-[5.5rem] bottom-[11rem] w-[18rem] xl:flex"
+                    title="Code generation"
+                  />
                 </ScrollParallax>
               </div>
             </div>
