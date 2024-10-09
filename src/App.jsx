08299cbcf14182eee205befcbf60e1bd5d02c47a
anonymous-lad2 <anonymouslad540@gmail.com>
2024-10-09 23:02:53 +0530

    Creating Benifit Section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import ButtonGradient from './assets/svg/ButtonGradient'
 import { Button } from './components/Button'
 import { Header } from './components/Header'
 import { Hero } from './components/Hero'
+import { Benifits } from './components/Benifits'
 
 const App = () => {
 
@@ -12,6 +13,7 @@ const App = () => {
       <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
         <Header />
         <Hero />
+        <Benifits />
       </div>
       <ButtonGradient />
     </>
---
+++ src/components/Benifits.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Heading } from './Heading'
+import { Section } from './Section'
+
+export const Benifits = () => {
+  return (
+    <Section id="features">
+      <div className="container relative z-2">
+        <Heading
+          className="md:max-w-md lg:max-w-2xl"
+          title="Chat Smarter, Not Harder with Brainwave"
+        />
+     </div>
+    </Section>
+  )
+}
